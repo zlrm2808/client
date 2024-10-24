f396c93a5372381d45e7471e2de9cfb47b3debf8
Zeus Rodriguez <zlrm2808@gmail.com>
2024-10-24 10:49:41 -0400

    food-App

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import "./global.css"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text className="text-red-500 text-4xl">code with Zeus</Text>
       <StatusBar style="auto" />
     </View>
   );
